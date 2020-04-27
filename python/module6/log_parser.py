cc5be5f3d3c5864d250098a385a167b1a46a13bc
Nikita Ivankov <IvankovNikita1992@gmail.com>
2020-04-27 20:09:50 +0300

    new function

--- python/module6/log_parser.py
+++ python/module6/log_parser.py
@@ -1,6 +1,23 @@
+import re
+from collections import Counter 
+
 class LogParser:
 	def __init__(self, log_name):
+		self.file = log_name
 
 	def get_most_common(self, top):
+		with open(self.file, 'r') as file:
+			list_ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", file.read())
+		
+		return (Counter(list_ip).most_common(top))
 
 	def log_by_http_code(self, output_file, code):
+		with open(output_file, 'w') as destination:
+			with open(self.file, 'r') as source:
+				for line in re.findall(rf'.+HTTP\/\d.\d. {code} .+', source.read()):
+					destination.write(line + '\n')
+
+# if __name__ == '__main__':
+# 	new = LogParser('access.log')
+# 	#print(new.get_most_common(5))
+# 	new.log_by_http_code('res.txt','200')
