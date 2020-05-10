ab0f8f7b2ccaed1dbf775fc6c282e6d2e35339a6
Nikita <ivankovnikita1992@gmail.com>
2020-05-10 22:52:54 +0300

    changes in ec_resources

--- python/module8/ec2_resource.py
+++ python/module8/ec2_resource.py
@@ -11,22 +11,18 @@ def change_instance_state(region, current_state, action):
             if action == 'start':
                 print(f'Starting instance {instance.id}')
                 ec2.Instance(instance.id).start()
-                ec2.Instance(instance.id).wait_until_running()
                 print('Completed')
             elif action == 'stop':
                 print(f'Stopping instance {instance.id}')
                 ec2.Instance(instance.id).stop()
-                ec2.Instance(instance.id).wait_until_stopped()
                 print('Completed')
             elif action == 'reboot':
                 print(f'Rebooting instance {instance.id}')
                 ec2.Instance(instance.id).reboot()
-                ec2.Instance(instance.id).wait_until_running()
                 print('Completed')
             elif action == 'terminate':
                 print(f'Terminating instance {instance.id}')
                 ec2.Instance(instance.id).terminate()
-                ec2.Instance(instance.id).wait_until_terminated()
                 print('Completed')
             else:
                 print(f"State of the instance {instance.id} is {instance.state['Name']} but you inputed unknown action....")
